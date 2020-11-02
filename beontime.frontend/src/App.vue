485ed3960b255a6c337d214d8ae3d48a05f261fd
Maciej Psiuk <maciek@psiuk.pl>
2020-11-02 11:47:41 +0100

    added NavigationDrawer.vue

--- beontime.frontend/src/App.vue
+++ beontime.frontend/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <Topbar />
+  <NavigationDrawer :is-open="drawerOpened" />
   <router-view/>
+  <font-awesome-icon type="fas" icon="bars" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Topbar from "@/components/Topbar.vue";
+import NavigationDrawer from "@/components/NavigationDrawer.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     Topbar,
+    NavigationDrawer,
   },
+  data: () => ({
+    drawerOpened: false,
+  }),
 });
 </script>
 
---
+++ beontime.frontend/src/components/NavigationDrawer.vue
@@ -0,0 +1,39 @@
+<template>
+  <aside
+    :class="isOpen ? 'translate-x-0' : '-translate-x-full'">
+  </aside>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavigationDrawer",
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  watch: {
+    isOpen: {
+      immediate: true,
+      handler (isOpen) {
+        if (process.client) {
+          if (isOpen) document.body.style.setProperty("overflow", "hidden");
+          else document.body.style.removeProperty("overflow");
+        }
+      },
+    },
+  },
+});
+</script>
+
+<style scoped lang="postcss">
+.topbar-nav {
+  @apply transform top-0 left-0 w-64;
+  @apply bg-white fixed h-full overflow-auto;
+  @apply ease-in-out transition-all duration-300 z-30;
+}
+</style>
