5c2af2927ea246041f8a824195dc47ad9a294e36
Maciej Psiuk <maciek@psiuk.pl>
2020-12-07 18:36:31 +0100

    changed BeButton.vue styling

--- beontime.frontend/src/libs/BeButton.vue
+++ beontime.frontend/src/libs/BeButton.vue
@@ -1,35 +1,32 @@
 <template>
     <component
-        :is="tag"
-        :type="type"
-        :disabled="disableButton"
-        class="bebutton"
-        :class="[btnClass, colorVariants]"
-        :variant="variant"
-        :variant-type="variantType"
-        :size="size"
-        :href="to"
-        v-bind="$attrs"
+        :is="componentTag"
+        class="be-button"
+        :to="to"
+        :href="href"
+        :color="color"
+        :disabled="disabled"
     >
-        <slot/>
+        <font-awesome-icon
+            v-if="icon"
+            :icon="icon"
+            class="be-button__icon"
+        />
+        <span
+            class="be-button__value"
+        >
+            <slot />
+        </span>
     </component>
 </template>
 
 <script lang='ts'>
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, computed, ref } from "vue";
 
-interface ButtonStyle {
-    normal: (isDisabled: boolean) => string,
-    outline: (isDisabled: boolean) => string,
-}
-
-interface ButtonStyles {
-    primary: () => ButtonStyle,
-    secondary: () => ButtonStyle,
-    danger: () => ButtonStyle,
-    success: () => ButtonStyle,
-    warning: () => ButtonStyle,
-    white: () => ButtonStyle
+interface Tags {
+    link: () => string;
+    anchor: () => string;
+    button: () => string;
 }
 
 export default defineComponent({
@@ -47,166 +44,113 @@ export default defineComponent({
             type: Boolean,
             default: false
         },
-        variant: {
-            type: String as PropType<string>,
-            default: "primary"
-        },
-        variantType: {
-            type: String,
-            default: "normal"
-        },
-        size: {
-            type: String,
-            default: "normal"
-        },
-        rounded: {
-            type: String,
-            default: "medium"
-        },
         type: {
+            type: String as PropType<string>,
+            default: "button"
+        },
+        color: {
+            type: String,
+            default: "default"
+        },
+        icon: {
             type: String,
             default: ""
-        },
-    },
-    data() {
-        return {
-            loading: false,
-            disableButton: this.disabled
-        };
-    },
-    computed: {
-        tag() {
-            if (this.href) {
-                return "a";
-            }
-            return "button";
-        },
-        btnClass(): any {
-            return {
-                "base-spinner": this.loading === true,
-                "cursor-not-allowed": this.disableButton === true,
-                "base-button inline-flex align-middle align-items-center justify-center font-medium focus:outline-none border-2": true,
-
-                "rounded-lg": this.rounded === "medium",
-                "rounded-full": this.rounded === "large",
-
-                "px-6 py-3": this.size === "normal",
-                "px-4 py-2": this.size === "small"
-            };
-        },
-        colorVariants(): string {
-            const variants: ButtonStyles = {
-                primary: (): ButtonStyle => ({
-                    normal: (isDisabled) => {
-                        if (isDisabled) {
-                            return "border-blue-300 bg-blue-300 text-white";
-                        }
-                        return "border-blue-600 bg-blue-600 hover:bg-blue-700 hover:border-blue-700 text-white";
-                    },
-                    outline: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "border-gray-200 text-blue-500 hover:text-blue-700";
-                    }
-                }),
-                secondary: (): ButtonStyle => ({
-                    normal: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "";
-                    },
-                    outline: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "border-gray-300 text-gray-500 hover:text-blue-500";
-                    }
-                }),
-                danger: (): ButtonStyle => ({
-                    normal: (isDisabled) => {
-                        if (isDisabled) {
-                            return "border-red-300 bg-red-300 text-white;";
-                        }
-                        return "border-red-600 bg-red-600 hover:bg-red-700 hover:border-red-700 text-white";
-                    },
-                    outline: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "border-gray-200 text-red-500 hover:text-red-600";
-                    }
-                }),
-                warning: (): ButtonStyle => ({
-                    normal: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "border-orange-600 bg-orange-600 hover:bg-orange-700 hover:border-orange-700 text-white";
-                    },
-                    outline: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "";
-                    }
-                }),
-                success: (): ButtonStyle => ({
-                    normal: (isDisabled) => {
-                        if (isDisabled) {
-                            return "border-green-300 bg-green-300 text-white";
-                        }
-                        return "border-green-600 bg-green-600 hover:bg-green-700 hover:border-green-700 text-white";
-                    },
-                    outline: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "border-2 border-gray-200 text-green-500 hover:text-green-700";
-                    }
-                }),
-                white: (): ButtonStyle => ({
-                    normal: (isDisabled) => {
-                        if (isDisabled) {
-                            return "border-white bg-white text-blue-600 hover:text-blue-800";
-                        }
-                        return "";
-                    },
-                    outline: (isDisabled) => {
-                        if (isDisabled) {
-                            return "";
-                        }
-                        return "";
-                    }
-                }),
-            };
-
-            const buttonStyle: ButtonStyle = variants[this.variant as keyof ButtonStyles]();
-
-            return buttonStyle[this.variantType as keyof ButtonStyle](this.disableButton);
         }
-    }
+    },
+    setup(props) {
+        const loading = ref(false);
+
+        const componentTag = computed((): string => {
+            const tags = {
+                link: () => "router-link",
+                anchor: () => "a",
+                button: () => "button",
+            } as Tags;
+
+            return tags[props.type as keyof Tags]();
+        });
+
+        return { loading, componentTag };
+    },
 });
 </script>
 
 <style lang='less' scoped>
-.bebutton {
+@default-color: #9f9f9f;
+@default-hover-color: #2c3e50;
+
+@primary-color: #22a7f0;
+@primary-hover-color: #0d7cb9;
+
+@success-color: #019875;
+@success-hover-color: #01654e;
+
+@warning-color: #f4b350;
+@warning-hover-color: #e9920f;
+
+@error-color: #d91e18;
+@error-hover-color: #941410;
+
+@disabled-color: #dadada;
+
+.button-color(@color, @hover-color) {
+    @apply "border-@{color}-600";
+    @apply "bg-@{color}-600";
+    @apply "text-white";
+
+    &:hover {
+        @apply "border-@{color}-700";
+        @apply "bg-@{color}-700";
+    }
+}
+
+.be-button {
+    cursor: pointer;
+    border: none;
+    background: 0 0;
+    @apply text-base;
+    @apply rounded-md;
+    text-decoration: none;
+
+    padding: 6px 24px;
+    outline: 0;
+
+    user-select: none;
     display: flex;
+    justify-content: center;
     align-items: center;
 
-    @apply p-4;
-    @apply shadow-xs;
-    @apply cursor-pointer;
     @apply transition;
     @apply duration-200;
 
-    &:focus {
-        outline: 0;
+    &::before, &::after {
+        content: '';
+        flex: 1 0 auto;
     }
 
-    &:hover {
-        @apply bg-indigo-700;
+    &:disabled {
+        cursor: not-allowed;
+        opacity: 0.333;
+    }
+
+    &[color="default"] {
+        .button-color(@default-color, @default-hover-color);
+    }
+
+    &[color="primary"] {
+        .button-color(@primary-color, @primary-hover-color);
+    }
+
+    &[color="success"] {
+        .button-color(@success-color, @success-hover-color);
+    }
+
+    &[color="warning"] {
+        .button-color(@warning-color, @warning-hover-color);
+    }
+
+    &[color="danger"] {
+        .button-color(@error-color, @error-hover-color);
     }
 }
 </style>
