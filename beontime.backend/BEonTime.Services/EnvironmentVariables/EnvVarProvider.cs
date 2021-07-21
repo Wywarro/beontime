d1eb791f11122da4edb033f70cd1146afdbe3adb
Maciej Psiuk <maciek@psiuk.pl>
2021-07-21 17:59:41 +0200

    removed all backend

--- beontime.backend/BEonTime.Services/EnvironmentVariables/EnvVarProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace BEonTime.Services.EnvironmentVariables
-{
-    public static class EnvVarProvider
-    {
-        public readonly static string SecretKey = Environment.GetEnvironmentVariable("SecretKey");
-    }
-}
